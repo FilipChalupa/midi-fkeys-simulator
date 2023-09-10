03a5b7f44efb0cfd8e506e75aad418667b888923
Filip Chalupa <chalupa.filip@gmail.com>
2023-09-10 19:18:52 +0200

    Base

---
+++ index.js
@@ -0,0 +1,21 @@
+import { execFileSync } from 'child_process'
+import easymidi from 'easymidi'
+import { exit } from 'process'
+
+const inputs = easymidi.getInputs()
+if (inputs.length === 0) {
+	console.error('No MIDI input found.')
+	exit(0)
+}
+const inputName = inputs.at(0)
+console.log(`Using ${inputName} as input.`)
+
+const input = new easymidi.Input(inputName)
+input.on('noteon', (message) => {
+	if (message.note >= 36 && message.note <= 43 && message.channel === 0) {
+		const index = message.note - 36
+		const key = `f${13 + index}`
+		console.log(`Simulate ${key} press.`)
+		execFileSync('nircmd.exe', ['sendkeypress', key])
+	}
+})
