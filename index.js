60c5c28a362c77b36c52d694d99ce66351767d86
Filip Chalupa <chalupa.filip@gmail.com>
2023-09-10 19:33:19 +0200

    Log: show time

--- index.js
+++ index.js
@@ -14,8 +14,13 @@ const input = new easymidi.Input(inputName)
 input.on('noteon', (message) => {
 	if (message.note >= 36 && message.note <= 43 && message.channel === 0) {
 		const index = message.note - 36
+		const now = new Date()
 		const key = `f${13 + index}`
-		console.log(`Simulate ${key} press.`)
+		console.log(
+			`[${now.toLocaleTimeString('en', {
+				hour12: false,
+			})}] Simulate ${key} press.`,
+		)
 		execFileSync('nircmd.exe', ['sendkeypress', key])
 	}
 })
